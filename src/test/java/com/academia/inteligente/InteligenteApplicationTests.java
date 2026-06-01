1b62f328eb902abef4ef6b35623af2f7405617d6
isaque_m_lopes <isaquem673@gmail.com>
2026-06-01 19:20:41 -0300

    first commit

---
+++ src/test/java/com/academia/inteligente/InteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
