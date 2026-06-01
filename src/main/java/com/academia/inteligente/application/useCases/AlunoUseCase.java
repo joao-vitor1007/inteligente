f7cc0c16910cd4f1795efde8d7e42d79f89f8bab
isaque_m_lopes <isaquem673@gmail.com>
2026-06-01 20:02:15 -0300

    usecases

---
+++ src/main/java/com/academia/inteligente/application/useCases/AlunoUseCase.java
@@ -0,0 +1,28 @@
+package com.academia.inteligente.application.useCases;
+
+import java.util.List;
+
+import com.academia.inteligente.application.gateways.AlunoGateway;
+import com.academia.inteligente.domain.Aluno;
+
+public class AlunoUseCase {
+
+    private final AlunoGateway gateway;
+
+    public AlunoUseCase(AlunoGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    public Aluno createAluno(Aluno aluno) {
+        return gateway.createAluno(aluno);
+    }
+
+    public List<Aluno> listaAluno() {
+        return gateway.listaAluno();
+    }
+
+    public void excluirAluno(Integer id) {
+        gateway.excluirAluno(id);
+    }
+
+}
--- src/main/java/com/academia/inteligente/application/useCases/TreinoUseCase.java
+++ src/main/java/com/academia/inteligente/application/useCases/TreinoUseCase.java
@@ -1,5 +1,28 @@
 package com.academia.inteligente.application.useCases;
 
+import java.util.List;
+
+import com.academia.inteligente.application.gateways.TreinoGateway;
+import com.academia.inteligente.domain.Treino;
+
 public class TreinoUseCase {
+
+    private final TreinoGateway gateway;
+
+    public TreinoUseCase(TreinoGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    public Treino createTreino(Treino treino) {
+        return gateway.createTreino(treino);
+    }
+
+    public List<Treino> listaTreino() {
+        return gateway.listaTreino();
+    }
+
+    public void excluirTreino(Integer id) {
+        gateway.excluirTreino(id);
+    }
 
 }
